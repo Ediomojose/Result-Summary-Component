b48aaec3d01f1dda0592e440127d77a715c1865c
Ediomojose <goodnewsediomojoshua@yahoo.com>
2024-01-07 13:12:14 +0100

    Creating components and folder structure

---
+++ src/Components/SummaryCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Assets/CSS/SummaryComponent.css'
+
+const SummaryCard = () => {
+  return (
+    <>
+    <main className='card--body'>
+        
+    </main>
+    </>
+  )
+}
+
+export default SummaryCard
